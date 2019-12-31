569c3433fbd2deab2777d1beab4f5749bf83e8bb
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 09:47:51 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/ArrayParameters.php
+++ test/TestAsset/ArrayParameters.php
@@ -1,15 +1,16 @@
 <?php
+
 /**
- * @see       https://github.com/zendframework/zend-expressive-template for the canonical source repository
- * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (https://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-template/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/mezzio/mezzio-template for the canonical source repository
+ * @copyright https://github.com/mezzio/mezzio-template/blob/master/COPYRIGHT.md
+ * @license   https://github.com/mezzio/mezzio-template/blob/master/LICENSE.md New BSD License
  */
 
 declare(strict_types=1);
 
-namespace ZendTest\Expressive\Template\TestAsset;
+namespace MezzioTest\Template\TestAsset;
 
-use Zend\Expressive\Template\ArrayParametersTrait;
+use Mezzio\Template\ArrayParametersTrait;
 
 class ArrayParameters
 {
