e915e5c81d5c65c092410775d7a25ce06cd3b4ba
George Steel <george@net-glue.co.uk>
2023-04-06 14:32:14 +0100

    Update psalm config with future defaults and fix or baseline unused code issues
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/ExceptionTest.php
+++ test/ExceptionTest.php
@@ -4,33 +4,27 @@ declare(strict_types=1);
 
 namespace MezzioTest\Template;
 
-use Generator;
 use Mezzio\Template\Exception\ExceptionInterface;
+use Mezzio\Template\Exception\InvalidArgumentException;
+use Mezzio\Template\Exception\RenderingException;
 use PHPUnit\Framework\TestCase;
 
-use function basename;
-use function glob;
 use function is_a;
-use function strrpos;
-use function substr;
 
 class ExceptionTest extends TestCase
 {
-    /** @psalm-return Generator<string, array<int, string>> */
-    public function exception(): Generator
+    /** @psalm-return array<class-string, array<int, class-string>> */
+    public static function exception(): array
     {
-        $namespace = substr(ExceptionInterface::class, 0, (int) strrpos(ExceptionInterface::class, '\\') + 1);
-
-        $exceptions = glob(__DIR__ . '/../src/Exception/*.php');
-        foreach ($exceptions as $exception) {
-            $class = substr(basename($exception), 0, -4);
-
-            yield $class => [$namespace . $class];
-        }
+        return [
+            InvalidArgumentException::class => [InvalidArgumentException::class],
+            RenderingException::class       => [RenderingException::class],
+        ];
     }
 
     /**
      * @dataProvider exception
+     * @param class-string $exception
      */
     public function testExceptionIsInstanceOfExceptionInterface(string $exception): void
     {
